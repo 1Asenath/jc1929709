4f004f88159af6f27da1c2d7b8a03c052f386e58
1Asenath <j.asenath@gmail.com>
2014-01-09 13:19:15 -0800

    Signed-off-by: 1Asenath <j.asenath@gmail.com>

---
+++ PearsonCustom_Chap1_Prob7_NB_010914/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Jennifer Clark
+ * Created on January 9, 2014, 12:52 PM
+ * CS! Computer Science/Cool Stuff
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //This program consists of output only
+cout<<"******************************************";
+cout<<endl;
+cout<<"       "<<"C"<<" "<<"C"<<" "<<"C"<<"          "<<"S"<<" "<<"S"<<" "<<"S"<<" "<<"S"<<"      !!"<<endl;
+cout<<"      "<<"C"<<"     "<<"C"<<"       "<<"S"<<"         "<<"S"<<"    !!"<<endl;
+cout<<"     C"<<"             "<<"S"<<"               !!"<<endl;
+cout<<"     C"<<"              "<<"S"<<"              !!"<<endl;
+cout<<"     C"<<"                "<<"S"<<" "<<"S"<<" "<<"S"<<" "<<"S"<<"      !!"<<endl;
+cout<<"     C"<<"                        "<<"S"<<"    !!"<<endl;
+cout<<"     C"<<"                         "<<"S"<<"   !!"<<endl;
+cout<<"      "<<"C"<<"     "<<"C"<<"       "<<"S"<<"         "<<"S"<<endl;
+cout<<"       "<<"C"<<" "<<"C"<<" "<<"C"<<"          "<<"S"<<" "<<"S"<<" "<<"S"<<" "<<"S"<<"      00"<<endl;
+cout<<endl;
+cout<<"******************************************";
+cout<<endl;
+cout<<"   Computer Science is Cool Stuff!!!";
+    return 0;
+}
+
