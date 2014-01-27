64be018863a700c23c82568bb09d17ff77af9aba
1Asenath <j.asenath@gmail.com>
2014-01-27 13:25:14 -0800

    Signed-off-by: 1Asenath <j.asenath@gmail.com>

---
+++ Chapter_5_HW/Savitch_8thEd_Chap5_Prob1_NB_012614/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Jennifer
+ *
+ * Created on January 26, 2014, 7:51 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ Chapter_5_HW/Savitch_8thEd_Chap5_Prob2_NB_012714/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Jennifer
+ *
+ * Created on January 27, 2014, 1:14 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
