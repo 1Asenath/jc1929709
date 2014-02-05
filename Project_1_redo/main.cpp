1552fb7b4345dcdcf0d98caef36574627534ba28
1Asenath <j.asenath@gmail.com>
2014-02-05 06:58:19 -0800

    Signed-off-by: 1Asenath <j.asenath@gmail.com>

--- Project_1_redo/main.cpp
+++ Project_1_redo/main.cpp
@@ -9,6 +9,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <ctime>
 using namespace std;
 
 //Global Constants
@@ -22,29 +24,90 @@ void haab(char); //output for head, body, arm, arm
 void haabl(char); //output for head, body, arm, arm
 void dead(char); //output for hanged man
 //input the word from the file
-string filewrd(string ,string ,string ,string ,string ,string);
+string filewrd(string [], string);
 
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Variables
-    string word, w1,w2,w3,w4,w5,w6;
+    string word, line ,filaray[200];
+    int hang=0, wrong=0;
+    char guess;
+    bool gamOver=false;
+    //Set Random Counter
+    srand(static_cast<unsigned int>(time(0)));
     //Intro
     cout<<"Welcome to Hangman!"<<endl;
     cout<<"To play, guess a letter. "<<endl;
-    //set up random drawing
-    word=filewrd(w1,w2,w3,w4,w5,w6);
-    cout<<word<<endl;
+    //Read in the file & generate a random word
+    word=filewrd(filaray, line);
+    //Output the random word
+    char cString[word.size()];
+    int arylngt=word.length();        //arylngt= array length
+    bool marker[word.size()];
+    for (int i=0;i<(arylngt-1);i++){
+    cString[i]=word[i];
+    marker[i]=false;
+    cout<<"__ ";
+    }
+    cout<<endl;
+    //Begin Game Loop
+    while(!gamOver){
+    cout<<"Type your letter guess (lowercase letters only): ";
+    cin>>guess;
+    bool ret=false;
+    for(int i=0;i<arylngt;i++){
+        if(cString[i]==guess)ret=true;
+    }
+    if(ret==true){
+        for(int i=0;i<(arylngt-1);i++){
+            if(marker[i]==true){
+                cout<<word[i]<<"  ";
+            }
+            else if(cString[i]==guess){
+                cout<<word[i]<<"  ";
+                marker[i]=true;
+            }
+            else if (cString[i]!=guess)
+                cout<<"__ ";        
+        }
+    }else{
+        cout<<"That letter is not found in the word!"<<endl;
+        hang++;
+    }
+    cout<<endl<<"You have used "<<hang<<"/6 chances"<<endl;
+    gamOver=true;
+    for(int i=0;i<(arylngt-1);i++){
+            if(marker[i]==false){
+                gamOver=false;
+            }
+    }
+    if(hang>5){
+        gamOver=true;
+    }
+    }
     return 0;
 }
 
-string filewrd(string w1,string w2,string w3,string w4,string w5,string w6){
+string filewrd(string filaray[], string line){
+    
+    short loop=0; //short for loop for input
+    // string line; //this will contain the data read from the file
     ifstream input;
     //Open the input file
     input.open("random_words.dat");
-    //Read in the table data
-    input>>w1>>w2>>w3>>w4>>w5;
-    return(w2);
+    if (input.is_open()){ //if the file is open
+        while (! input.eof() ){ //while the end of file is NOT reached
+            getline(input,line); //get one line from the file
+            filaray[loop]=line;
+            //TEST cout<<filaray[loop]<<endl; //output the array
+            loop++;
+        }
+        input.close(); //closing the file
+    }
+    int randnum=rand()%loop;
+    return (filaray[randnum]);
 }
+
 
 void blank(char x){
 cout<<"    x x x x x x"<<endl;
